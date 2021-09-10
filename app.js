6f7216a0c83204ce2d0d94df9bd1e34c8357982a
John <jgeiger48@gmail.com>
2021-09-10 17:39:29 -0600

    Passport

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 // lines 14 through 26 establish connection to Mongodb server
 const mongoose = require('mongoose');
@@ -46,28 +48,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
-      const err = new Error('You are not authenticated!');
+  if (!req.user) {
+      const err = new Error('You are not authenticated!');                    
       err.status = 401;
       return next(err);
   } else {
-      if (req.session.user === 'authenticated') {
-          return next();
-      } else {
-          const err = new Error('You are not authenticated!');
-          err.status = 401;
-          return next(err);
-      }
+      return next();
   }
 }
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
