3c7230f6a72844ac21a2af018eaf6babf7b261ac
John <jgeiger48@gmail.com>
2021-09-17 20:13:45 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '237163941555283',
+        clientSecret: '2ffcff079eaf28d7598a4abbc6bc32fd'
+    }
 }
